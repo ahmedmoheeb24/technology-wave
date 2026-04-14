468f01d97cec58958743e5c7a750263de3594a16
darvenproject <shopdarven@gmail.com>
2026-04-14 23:14:42 +0500

    .

--- client/assets/assets.js
+++ client/assets/assets.js
@@ -1,21 +1,22 @@
 export const assets = {
   logo: '/logo.png',
   about: {
+    // Note the spaces here to match your folder
     aircraft: '/about - aircraft.jfif',
     part: '/about - part.jfif',
   },
   services: {
-    commercialAviation1: '/commercial-aviation-1.jfif',
+    commercialAviation1: '/Commercial Aviation 1.jfif',
     commercialAviation2: '/commercial-aviation-2.jfif',
-    militaryDivision1: '/military-division-1.jfif',
+    militaryDivision1: '/Military Division 1.jfif',
     militaryDivision2: '/military-division-2.jfif',
-    helicopters1: '/helicopters-1.jfif',
+    helicopters1: '/Helicopters 1.jfif',
     helicopters2: '/helicopters-2.jfif',
-    aircraftMaintenance1: '/aircraft-maintenance-1.jfif',
+    aircraftMaintenance1: '/Aircraft Maintenance 1.jfif',
     aircraftMaintenance2: '/aircraft-maintenance-2.jfif',
-    aircraftSolutions1: '/aircraft-solutions-1.jfif',
+    aircraftSolutions1: '/Aircraft Solutions 1.jfif',
     aircraftSolutions2: '/aircraft-solutions-2.jfif',
-    aircraftParts1: '/aircraft-parts-1.jfif',
+    aircraftParts1: '/Aircraft Parts 1.jfif',
     aircraftParts2: '/aircraft-parts-2.jfif',
   }
 };
